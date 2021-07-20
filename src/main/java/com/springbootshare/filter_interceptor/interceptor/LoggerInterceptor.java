f4e52bfbafeb07ba38db745310b08e77433be47e
felicityamiao <1760077814@qq.com>
2021-07-20 11:47:17 +0800

    add interceptor

---
+++ src/main/java/com/springbootshare/filter_interceptor/interceptor/LoggerInterceptor.java
@@ -0,0 +1,28 @@
+package com.springbootshare.filter_interceptor.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggerInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("LoggerInterceptor: preHandle: " + request.getRequestURI());
+        return true; //continue next step
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("LoggerInterceptor: postHandle: " + request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("LoggerInterceptor: afterCompletion: " + request.getRequestURI());
+    }
+}
